8a63a33ecdb00ca54f8ee32bb201b6c057d547c4
yoonvelop <yoonvelop@gmail.com>
2024-03-12 21:52:20 +0900

    feat: 과제2 - 입력폼UI 개발

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import './styles/reset.css';
+import './styles/common.css';
 import React from 'react';
 import { cardsData } from './constants/card';
 import CardList from './components/cards/CardList';
+import TextField from './components/TextField';
 
 function App() {
   return (
     <div className="App">
-      <CardList cards={cardsData} />
+      <main>
+        <h1>과제 1</h1>
+        <CardList cards={cardsData} />
+        <h1>과제 2</h1>
+        <TextField
+          initialText="[default] 초깃값이 있을 수 있습니다."
+          maxLength={500}
+          disabled={false}
+        />
+        <TextField
+          initialText=""
+          disabledPlaceholder={'[disable] 주문 요청사항을 입력해 주세요.'}
+          maxLength={500}
+          disabled={true}
+        />
+        <TextField
+          initialText="[readonly] 읽기 전용 상태"
+          maxLength={500}
+          readOnly={true}
+        />
+      </main>
     </div>
   );
 }
